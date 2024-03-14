298393a241d2ad48ec0425103e325d8c0bc07910
Nicholas027 <nicholas0810152015@gmail.com>
2024-03-14 11:21:30 -0300

    listado y edicion de productos

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -4,29 +4,57 @@ export const listarProductos = async(req, res)=>{
     try{
         const productos = await Producto.find();
         res.status(200).json(productos)
-
     }catch(error){
-        console.log(error)
+        console.error(error);
+        res.status(404).json({
+            mensaje: "Los productos no se pudieron obtener",
+        })
     }
 }
 
 export const crearProducto = async(req, res)=>{
     try {
-        //extraer los datos del body
-        console.log(req.body);
-        //todo: agregar la validacion de los datos del body
         const productoNuevo = new Producto(req.body);
-        //pedirle a la BD guardar el producto nuevo
         await productoNuevo.save();
-        //enviar la respuesta al front
+
         res.status(201).json({
             mensaje: "El producto fue creado correctamente"
         })
         
     } catch (error) {
-        console.log(error);
+        console.error(error);
         res.status(400).json({
             mensaje: "El producto no pudo ser dado de alta"
+        })
+    }
+}
+
+export const obtenerProducto = async (req, res) => {
+    try {
+        const productoBuscado = await Producto.findById(req.params.id);
+
+        res.status(200).json(productoBuscado)
+    } catch (error) {
+        console.error(error);
+        res.status(404).json({
+            mensaje: "El producto no pudo ser encontrado"
+        })
+    }
+}
+
+export const editarProducto = async (req, res) => {
+    try {
+        const productoBuscado = await Producto.findById(req.params.id);
+
+        if(!productoBuscado) return res.status(404).json({mensaje: "El producto no fue encontrado!"});
+
+        const productoEditado = await Producto.findByIdAndUpdate(req.params.id, req.body);
+
+        res.status(200).json({mensaje: "El producto fue editado correctamente", productoEditado})
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            mensaje: "Ha ocurrido un error en el servidor!"
         })
     }
 }
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { crearProducto, listarProductos } from "../controllers/productos.controllers.js";
+import { crearProducto, editarProducto, listarProductos, obtenerProducto } from "../controllers/productos.controllers.js";
 
 const router = Router();
 
 router.route('/productos').get(listarProductos).post(crearProducto)
+router.route('/productos/:id').get(obtenerProducto)
+router.route('/productos/:id').put(editarProducto)
 
 export default router;
